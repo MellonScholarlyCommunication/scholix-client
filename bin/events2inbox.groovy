5a01cd015252cedfebee31354c200fb22fc44684
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-04-21 10:14:18 +0200

    Adding script to post event notifications

---
+++ bin/events2inbox.groovy
@@ -0,0 +1,36 @@
+#!/usr/bin/env groovy -cp ./lib
+
+import scholix.ScholixClient
+import groovy.json.JsonOutput
+import java.net.HttpURLConnection
+
+if (args.size() == 0) {
+    System.err.println("usage: events2inbox.groovy file")
+    System.exit(1)
+}
+
+new ScholixClient().cache_loop(args[0], {
+    x -> sendNotification(x)
+})
+
+def sendNotification(evt) {
+    def targetInbox = evt['target']['inbox']
+
+    def notification = JsonOutput.prettyPrint(JsonOutput.toJson(evt))
+
+    def url = new URL(targetInbox)
+    def connection = (HttpURLConnection) url.openConnection()
+
+    connection.with {
+        setDoOutput(true)
+        setRequestMethod('POST')
+        setRequestProperty('Accept','text/turle')
+        setRequestProperty('Content-Type','application/ld+json')
+        setRequestProperty('Content-Length',"" + notification.getBytes().length)
+        outputStream.withWriter { writer ->
+            writer << notification
+        }
+
+        println(responseCode + " " + targetInbox)
+    }
+}
